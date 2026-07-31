1f640eb5f4f84fc5888744ad7fa5b8d09b252d53
Shakila Jahan Tasnia <sakilajahantasnia@gmail.com>
2026-07-31 23:13:56 +0600

    create NoteFactory.java
    Implemented the **Factory Design Pattern** by creating a **NoteFactory** class to handle the creation of **Note** objects. Instead of creating notes directly, the **createNote()** method sets all required fields, including the title, content, subject, user, and default values for favorite and pinned status. This reduces duplicate code, keeps the project organized, and makes future changes to note creation easier.

--- src/main/java/com/noteorg/factory/NoteFactory.java
+++ src/main/java/com/noteorg/factory/NoteFactory.java
@@ -1,1 +1,31 @@
+package com.noteorg.factory;
 
+import com.noteorg.model.Note;
+import com.noteorg.model.Subject;
+import com.noteorg.model.User;
+
+public class NoteFactory {
+
+    // Private constructor to prevent creating objects of this class
+    private NoteFactory() {
+    }
+
+     // Creates and initializes a Note object.
+  
+    public static Note createNote(String title, String content,
+                                  Subject subject, User user) {
+
+        Note note = new Note();
+
+        note.setTitle(title);
+        note.setContent(content);
+        note.setSubject(subject);
+        note.setUser(user);
+
+        // Default values
+        note.setFavorite(false);
+        note.setPinned(false);
+
+        return note;
+    }
+}
